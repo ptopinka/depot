fee4dca7589c1095889d9e66aac43b112e568ff6
Pavel Topinka <ptopinka@gmail.com>
2011-05-06 23:07:49 +0200

    Validations

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "product attributes must not be empty" do
+   product = Product.new
+   assert product.invalid?
+   assert product.errors[:title].any?
+   assert product.errors[:description].any?
+   assert product.errors[:price].any?
+   assert product.errors[:image_url].any?
   end
+
+
+  test "product must be possitive" do
+    product = Product.new(:title  =>"The girl with dragon tatoo",
+                          :description => "good story",
+                          :image_url => "image.png"
+                          )
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+  end
+
+
+
 end
